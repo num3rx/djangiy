4ae1a480588f04d87780e5d72e7e5765f16f0c00
num3rx <sachat2006@gmail.com>
2023-08-09 22:43:46 +0500

    Сделано дз 8 занятия

--- adver/app_adver/admin.py
+++ adver/app_adver/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Advertisements
 
 # Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_at', 'updated_at', 'auction']
+
+    fieldsets = (
+        (
+            'Общее', {
+                'fields': ('title', 'description')
+            }
+        ),
+        (
+            'Финансы', {
+                'fields': ('price', 'auction'),
+                'classes': ['collapse']
+            }
+        )
+    )
+
+    list_filter = ['auction', 'created_at']
+
+    actions = ['make_auction_disabled', 'make_auction_enabled']
+
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_disabled(self, request, queryset):
+        queryset.update(auction=False)
+
+
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_enabled(self, request, queryset):
+        queryset.update(auction=True)
+
+
+admin.site.register(Advertisements, AdvertisementAdmin)
--- adver/app_adver/apps.py
+++ adver/app_adver/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppAdverConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_adver'
+    verbose_name = 'Объявления'
